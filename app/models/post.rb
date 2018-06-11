a92d1620e5ebd42174e2c54e5ac223e8b4be30cd
github username <github email address>
2018-06-11 15:48:42 -0400

    tabled custom validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
+class MyValidator < ActiveModel::Validator
+  def validate(post)
+    post_down = post.title.downcase
+    unless !(post_down.include?("won't believe") || post_down.include?("secret") || post_down.include?("guess") || post_down.match(/top [0123456789]/))
+      record.errors[:title] << 'Clickbait detected.'
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :summary, length: { maximum: 200 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
+
+  # include ActiveModel::Validations
+  # validates_with MyValidator
 end
